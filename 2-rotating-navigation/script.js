ce6518af58c76543fc54cefcd6b5818b73c9159f
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-07-29 14:20:36 +0300

    Add files via upload

---
+++ 2-rotating-navigation/script.js
@@ -0,0 +1,9 @@
+const open = document.querySelector(".open");
+const close = document.querySelector(".close");
+const container = document.getElementById("outer-container");
+open.addEventListener("click", () => {
+    container.classList.add("rotate")
+});
+close.addEventListener("click", () => {
+    container.classList.remove("rotate")
+});
